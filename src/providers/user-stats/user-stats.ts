97c6021770dd75aac721edd94ee191b595df21e2
Dominic Evans <dominic@bitt.com>
2018-10-31 19:54:33 -0400

    remove reolve from dns in user-stats

--- src/providers/user-stats/user-stats.ts
+++ src/providers/user-stats/user-stats.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { resolve } from 'dns';
+// import { resolve } from 'dns';
 
 @Injectable()
 export class UserStatsProvider {
